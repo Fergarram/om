b20fb15cd4db458a57ab2e52f37e9209a1019743
Fernando Garcia <fergarram@gmail.com>
2025-11-01 20:46:07 -0600

    fix

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const PORT = 1996;
-const PUBLIC_DIR = path.join(__dirname, "public");
+const PUBLIC_DIR = "/home/fernando/public";
 
 const MIME_TYPES = {
 	".html": "text/html",
